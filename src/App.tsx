443e47193853c0100895510e4f4902d5983e512c
Tomas Ciernik <Tciernik@outlook.com>
2023-04-07 09:49:40 +0100

    added types for typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import Topbar from './views/global/Topbar';
+import Sidebar from './views/global/Sidebar';
+import Dashboard from './views/dashboard/Dashboard';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { Routes,Route } from 'react-router-dom';
+
+
+//themeprociver passing the themes to MUI components
 
 const App:React.FC = () =>{
+
+  const {theme,colorMode} = useMode();
+
   return (
-    <div className="app">
-      New App
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar/>
+            <Routes>
+              <Route path="/" element={<Dashboard/>}/>
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>    
+    </ColorModeContext.Provider>
   );
 }
 
