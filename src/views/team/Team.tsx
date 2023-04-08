c522664da4a5570eac9c00bef82c00e53f472d67
Tomas Ciernik <Tciernik@outlook.com>
2023-04-08 13:11:31 +0100

    datagrid styled

--- src/views/team/Team.tsx
+++ src/views/team/Team.tsx
@@ -24,7 +24,7 @@ const colors = tokens(theme.palette.mode);
 
     const columns:any[] =[
     {field: "id", headerName: "ID"},
-    {field: "name", headerName: "Name", flex: 1,cellClassName: "name-column--cell"},
+    {field: "name", headerName: "Name", flex: 1,cellClassName: "name-column--cell"}, // className used down in the code in custom Box style
     {field: "age", headerName: "Age", type:"number",headerAlign: "center",align: "left"},
     {field: "phone", headerName: "Phone Number",flex:1},
     {field: "email", headerName: "Email",flex:1},
@@ -67,6 +67,28 @@ return (
         <Box
             m="40px 0 0 0"
             height="75vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                  border:"none"
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom:"none"
+              },
+              "& .name-column--cell": {
+                color:colors.greenAccent[300]
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor:colors.blueAccent[700],
+                borderBottom:"none"
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor:colors.primary[400],
+              },
+              "& .MuiDataGrid-footerContainer": {
+                borderTop:"none",
+                backgroundColor:colors.blueAccent[700],
+              },
+            }}
         >
             <DataGrid columns={columns} rows={mockDataTeam}
             />
